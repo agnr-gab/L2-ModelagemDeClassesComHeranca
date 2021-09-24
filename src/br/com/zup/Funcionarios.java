c112ae250cb5480f5064ac2630b23115f5e65684
Arê Nascimento <aretuza.gab@gmail.com>
2021-09-24 16:10:28 -0300

    Classe funcionario

---
+++ src/br/com/zup/Funcionarios.java
@@ -0,0 +1,61 @@
+package br.com.zup;
+
+/*
+Nome, CPF, número de
+registro, órgão de lotação e salário.
+ */
+public class Funcionarios {
+    private String nome;
+    private String cpf;
+    private String numeroRegistro;
+    private String orgaoLotacao;
+    private double salario;
+
+    public Funcionarios(String nome, String cpf, String numeroRegistro, String orgaoLotacao, double salario) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.numeroRegistro = numeroRegistro;
+        this.orgaoLotacao = orgaoLotacao;
+        this.salario = salario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNumeroRegistro() {
+        return numeroRegistro;
+    }
+
+    public void setNumeroRegistro(String numeroRegistro) {
+        this.numeroRegistro = numeroRegistro;
+    }
+
+    public String getOrgaoLotacao() {
+        return orgaoLotacao;
+    }
+
+    public void setOrgaoLotacao(String orgaoLotacao) {
+        this.orgaoLotacao = orgaoLotacao;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+}
