f3d2e79a2a4b74f3b9bdf613917b469dcc3c9d97
Arê Nascimento <aretuza.gab@gmail.com>
2021-09-26 21:59:05 -0300

    Classe funcionarios administrativos getters e setters

--- src/br/com/zup/FuncionariosAdministrativos.java
+++ src/br/com/zup/FuncionariosAdministrativos.java
@@ -1,4 +1,28 @@
 package br.com.zup;
 
-public class FuncionariosAdministrativos {
+public class FuncionariosAdministrativos extends Funcionarios{
+    private String funcaoAdm;
+    private String senioridade;
+
+    public FuncionariosAdministrativos(String nome, String cpf, String numeroRegistro, String orgaoLotacao, double salario, String funcaoAdm, String senioridade) {
+        super(nome, cpf, numeroRegistro, orgaoLotacao, salario);
+        this.funcaoAdm = funcaoAdm;
+        this.senioridade = senioridade;
+    }
+
+    public String getFuncaoAdm() {
+        return funcaoAdm;
+    }
+
+    public void setFuncaoAdm(String funcaoAdm) {
+        this.funcaoAdm = funcaoAdm;
+    }
+
+    public String getSenioridade() {
+        return senioridade;
+    }
+
+    public void setSenioridade(String senioridade) {
+        this.senioridade = senioridade;
+    }
 }
